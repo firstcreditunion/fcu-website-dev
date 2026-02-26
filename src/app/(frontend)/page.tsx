d61733c6ce08555ea07d37c7727beb08b37fec40
Isaac Vicliph <gateway@firstcu.co.nz>
2026-02-26 16:18:29 +1300

    Updated Nav

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       title="Welcome to First Credit Union"
       isHomepage
       description="We are building something amazing. Our new website is under development and will be launching soon with a fresh experience for our members."
-      highlightWords={['something amazing', 'launching soon']}
     />
   )
 }
